4fb5d20000e5b0118a39f90f279b130d1020634e
Charan-SequoiaAT <charan@sequoiaat.com>
2022-09-19 11:12:37 +0530

    commit at 11:12 AM

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className='table-responsive'>
     <Form>
       <input type={password}></input>
       <input type={password}></input>
+      <input type={password}></input>
+      <input type={password}></input>
     </Form>
-    </>
+    </div>
   );
 }
 
